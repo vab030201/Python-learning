d1a63c38b6ca89d8a689d230bf3d87d0a30dcfbb
Vibhavari Borole <vab030201@gmail.com>
2026-09-02 01:26:10 +0530

    datatypes

---
+++ Datatypes/main.py
@@ -0,0 +1,29 @@
+#integer
+print( 6 )
+
+#float 
+print( 6.0 )
+print( 1.7e308 )
+
+#string 
+print("Hello Universe")
+
+
+#container types 
+#list 
+print([1, 2, 3])
+
+#tuple
+print((1, 2, 3))
+
+#set
+print({1, 2, 3})
+
+#dictionary- searchable key-value pairs
+print({"name": "John", "age": 30, "city": "New York"})
+
+# comments- a peice of text that is not executed by the interpreter. 
+# It is used to explain the code and make it more readable.
+# Comments are preceded by a # symbol.
+# enhance code readability and maintainability by providing context and explanations for complex logic or algorithms. 
+
