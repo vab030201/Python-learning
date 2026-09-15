30e02093e73bed2c50338073b016f21bd92479b4
Vibhavari Borole <vab030201@gmail.com>
2026-09-16 02:39:27 +0530

    2.md

---
+++ tryouts/PythonDemos2md/ifelifelse.py
@@ -0,0 +1,11 @@
+
+marks = int(input("Enter Marks: "))
+
+if marks >= 75:
+    print("Distinction")
+elif marks >= 60:
+    print("First Class")
+elif marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
---
+++ tryouts/PythonDemos2md/ifelse.py
@@ -0,0 +1,7 @@
+
+marks = int(input("Enter Marks: "))
+
+if marks >= 40:
+    print("Congratulations! You Passed.")
+else:
+    print("Better Luck Next Time.")
