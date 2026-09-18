8c8649d889d500369520afa2608f18b2de984a69
Vibhavari Borole <vab030201@gmail.com>
2026-09-18 11:47:59 +0530

    Updatedoop

---
+++ tryouts/PythonDemo5md/OOPs/Polymorphism.py
@@ -0,0 +1,26 @@
+
+class LifePolicy:
+
+    def calculate_premium(self):
+        return 25000
+    
+    
+class HealthPolicy:
+
+    def calculate_premium(self):
+        return 18000
+    
+    
+class MotorPolicy:
+
+    def calculate_premium(self):
+        return 12000
+    
+policies = [
+    LifePolicy(),
+    HealthPolicy(),
+    MotorPolicy()
+]
+
+for policy in policies:
+    print(policy.calculate_premium())            
